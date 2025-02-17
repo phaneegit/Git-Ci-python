c5daa519d676273f8de7d30749473978fc907ed8
phanee-git <phanindravangavolu96@gmail.com>
2025-02-17 04:28:10 +0000

    include version

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from version import VERSION  # Import version
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello,Welcome to Git_CI!"
+    return f"Hello, Welcome to Git_CI! (Version: {VERSION})"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
